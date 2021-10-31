51339c411c18720a891c8edb5512985f1776f772
Ilham1m4man <kucingdiki@gmail.com>
2021-10-31 13:55:17 +0700

    add file Tabungan.java

---
+++ src/com/ilham/pbo/modul6/percobaan/Tabungan.java
@@ -0,0 +1,56 @@
+package com.ilham.pbo.modul6.percobaan;
+
+public class Tabungan {
+    private String nama;
+    private int noRekening;
+    private int saldo;
+    private int pin;
+
+    public Tabungan(String nama, int noRekening){
+        this.nama = nama;
+        this.noRekening = noRekening;
+        this.saldo = 15000;                 // Nilai default saldo
+        this.pin = 11111;               // Nilai default PIN
+    }
+
+    public void simpanUang(int jumlah){
+        this.saldo += jumlah;
+    }
+
+    public void tarikUang(int jumlah){
+        this.saldo -= jumlah;
+    }
+
+    public void setSaldo(int saldo){
+        this.saldo = saldo;
+    }
+
+    public void setPin(int pin){
+        this.saldo += pin;
+    }
+
+    public Tabungan(String nama, int noRekening, int saldo, int pin){
+        this.nama = nama;
+        this.noRekening = noRekening;
+        this.saldo = saldo;
+        this.pin = pin;
+    }
+
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public int getNoRekening() {
+        return this.noRekening;
+    }
+
+    public int getSaldo() {
+        return this.saldo;
+    }
+
+    public int getPin() {
+        return this.pin;
+    }
+
+}
