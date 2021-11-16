5a52de98a9a7dcfd8ff3781815e9875b340ec12f
Ilham1m4man <kucingdiki@gmail.com>
2021-11-16 16:56:04 +0700

    add file House.java

---
+++ src/com/ilham/pbo/modul7/percobaan/House.java
@@ -0,0 +1,22 @@
+package com.ilham.pbo.modul7.percobaan;
+
+public class House {
+    public static void main(String[] args) {
+        TV tv = new TV("TV");
+        AC ac = new AC("AC");
+        Electronics lamp = new Electronics("Lamp");
+        Electronics device = new Electronics();
+
+        tv.activate();
+        ac.activate();
+        lamp.activate();
+        device.activate();
+
+        System.out.println();
+
+        device.deactivate();
+        lamp.deactivate();
+        ac.deactivate();
+        tv.deactivate();
+    }
+}
